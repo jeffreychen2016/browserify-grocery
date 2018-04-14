4f47a4976d13c2ac27a0ef41906326c92040ac12
jeffrey chen <jeffreychen2016@gmail.com>
2018-04-14 12:17:40 -0500

    Added events

--- javascripts/doms.js
+++ javascripts/doms.js
@@ -1,14 +1,17 @@
+const events = require('./events');
+
 const printToDom = (strang,domID) => {
   document.getElementById(domID).innerHTML = strang;
+  events.addDepartmentEvents();
 };
 
 const builDomString = (departmentArray) => {
   let strang = '';
   strang += `<div class='row text-center'>`;
-  departmentArray.forEach((deparment) => {
+  departmentArray.forEach((department) => {
     strang +=   `<div class='col-md-3 department'>`;
-    strang +=    `<h3 class='hide'>${deparment.name}</h3>`;
-    strang +=    `<img src="${deparment.img}" class='department-img'>`;
+    strang +=    `<h3 class='hide department-title'  data-department-id='${department.id}'>${department.name}</h3>`;
+    strang +=    `<img src="${department.img}" class='department-img'>`;
     strang +=   `</div>`;
   });
   strang += `<div>`;
